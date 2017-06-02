2951bba20637585ee22ee63c415adfcc905e034f
Lukasz Ludwin <gorli@interia.pl>
2017-06-02 22:36:57 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+﻿function checkOnclickEvent(param) {
+  console.log(param);
+}
+
+var inputElem = document.getElementById('js-inputValue'),
+    imageElem = document.getElementById('js-image');
+    add = document.getElementById("addElem");
+    list = document.getElementById("list");
+
+    imageElem.addEventListener('click', function(e) {
+  checkOnclickEvent('image was clicked');
+});
+
+inputElem.addEventListener('keypress', function(e) {
+  e.target.value += ' test ';
+});
+
+add.addEventListener('click', function() {
+	var element = document.createElement('li');
+
+	element.innerHTML = 'item ' + document.getElementsByTagName("li").length;
+	list.appendChild(element);
+});
+
